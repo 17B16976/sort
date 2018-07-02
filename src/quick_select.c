41e9dea33834770b70734fdbe54017292f499015
WadaTakuya <wada.t.ap@m.titech.ac.jp>
2018-07-02 17:05:57 +0900

    難しい

--- src/quick_select.c
+++ src/quick_select.c
@@ -25,18 +25,19 @@ int quick_select(int A[], int n, int k){
 
   if(j == k+1) return pivot;
   else if(j < k+1) return quick_select(A+j, n-j, k-j);
-  else return quick_select(A+1, j-1, k);
+  else return quick_select(A+1, j - 1, k);
 }
 
 int main(){
-  int i;
-  A[0] = 0;
-  A[1] = 3; //原始元
-  for(i=2;i<N;i++){
-    A[i] = (long long int) A[i-1] * A[1] % N;
-  }
-  for(i=0;i<N;i++){
-    if(quick_select(A, N, i) != i) printf("ERROR %d %d\n", i, quick_select(A, N, i));
-//    printf("%d th element is %d\n", i, quick_select(A, N, i));
-  }
+    int i ;
+    for(i=0;i<N;i++){
+        A[i] = (long long int) 0 ;
+    }
+    
+    // すべての要素が同じ場合でも計算が早く終わるか確認する
+    
+    for(i=0;i<N;i++){
+        if(quick_select(A, N, i) != 0) printf("ERROR %d %d\n", i, quick_select(A, N, i));
+        //    printf("%d th element is %d\n", i, quick_select(A, N, i));
+    }
 }
--- src/quick_select_improved.c
+++ src/quick_select_improved.c
@@ -10,36 +10,41 @@ A[0], A[1], ..., A[n-1] の中でk+1番目に小さい値を返す関数
 ただし、Aの中身は書き換えてしまう。
 */
 int quick_select(int A[], int n, int k){
-  int i, j, pivot;
+  int i, j, b, pivot;
 
 // 先頭の要素をピボットとする
   pivot = A[0];
-  for(i = j = 1; i < n; i++){
-    if(A[i] <= pivot){
-      int z = A[j];
-      A[j] = A[i];
-      A[i] = z;
-      j++;
+  for(i = j = b = 1; i < n; i++){
+    if(A[i] < pivot){
+        int z = A[j];
+        A[j] = A[i];
+        A[i] = z;
+         j++;
+    }
+    else if(A[i] == pivot) {
+        int z = A[j];
+        A[j] = A[i];
+        A[i] = z;
+        j++;
+        b++;
     }
   }
 
-  if(j == k+1) return pivot;
+  if(j-b+1 <= k+1 && k+1 <= j) return pivot;
   else if(j < k+1) return quick_select(A+j, n-j, k-j);
   else return quick_select(A+1, j-1, k);
 }
 
 int main(){
-  int i;
-  A[0] = 0;
-  A[1] = 3; //原始元
-  for(i=2;i<N;i++){
-    A[i] = (long long int) A[i-1] * A[1] % N;
+    int i ;
+    for(i=0;i<N;i++){
+      A[i] = (long long int) 0 ;
   }
 
 // すべての要素が同じ場合でも計算が早く終わるか確認する
 
   for(i=0;i<N;i++){
-    if(quick_select(A, N, i) != i) printf("ERROR %d %d\n", i, quick_select(A, N, i));
+    if(quick_select(A, N, i) != 0) printf("ERROR %d %d\n", i, quick_select(A, N, i));
 //    printf("%d th element is %d\n", i, quick_select(A, N, i));
   }
 }
